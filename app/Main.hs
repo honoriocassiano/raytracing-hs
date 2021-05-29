7c70048f77a5a6009e7f609e873607df60749d8e
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2021-05-29 17:15:19 -0300

    Use linux file ending

--- app/Main.hs
+++ app/Main.hs
@@ -15,7 +15,7 @@ generate height width = mapM_ (\(i, j) -> do
     let pi = (fromIntegral i) / (fromIntegral height-1)
     let pj = (fromIntegral j) / (fromIntegral width-1)
     let (px, py, pz) = pixel pi pj
-    putStrLn $ printf "%d %d %d" px py pz) [(i, j) | i <- [0..height-1], j <- [0..width-1]]
+    putStr $ printf "%d %d %d\n" px py pz) [(i, j) | i <- [0..height-1], j <- [0..width-1]]
 
 main :: IO ()
 main = generate 2 2
