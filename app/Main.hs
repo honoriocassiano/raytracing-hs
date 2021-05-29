cdb9256be4f62df7900e4f93d6a85e407e62ad66
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2021-05-29 19:12:14 -0300

    Big refactor to write image to file

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,34 @@
 module Main where
 
 import Control.Monad
+import Data.List
 import GHC.Float.RealFracMethods
 import Text.Printf
 
 convert :: Float -> Integer
 convert v = truncateFloatInteger $ v * 255.999
 
-pixel :: Float -> Float -> (Integer, Integer, Integer)
-pixel pi pj = (convert pi, convert pj, convert 0.25)
+pixel :: Float -> Float -> [Integer]
+pixel pi pj = [convert pi, convert pj, convert 0.25]
 
-generateHeader :: Integer -> Integer -> IO ()
+generateHeader :: Integer -> Integer -> [String]
 generateHeader height width =
-    putStr $ printf "P3\n%d %d\n255\n" width height
+    ["P3", (show width) ++ " " ++ (show height), "255"]
 
-generatePixels :: Integer -> Integer -> IO ()
-generatePixels height width = mapM_ (\(i, j) -> do
+generatePixels :: Integer -> Integer -> [String]
+generatePixels height width = map (\(i, j) -> do
+
     let pi = (fromIntegral i) / (fromIntegral height-1)
     let pj = (fromIntegral j) / (fromIntegral width-1)
-    let (px, py, pz) = pixel pi pj
-    putStr $ printf "%d %d %d\n" px py pz) [(i, j) | i <- [0..height-1], j <- [0..width-1]]
+    let pix = map (\p -> show p) $ pixel pi pj
 
-generate :: Integer -> Integer -> IO ()
+    intercalate " " pix) [(i, j) | i <- [0..height-1], j <- [0..width-1]]
+
+generate :: Integer -> Integer -> [String]
 generate height width = do
-    generateHeader height width
-    generatePixels height width
+    (generateHeader height width) ++ (generatePixels height width)
 
 main :: IO ()
-main = generate 256 256
+main = do
+    let content = intercalate "\n" $ generate 256 256
+    writeFile "image.ppm" content
