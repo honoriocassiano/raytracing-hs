f0a022b3be5cd46685bbecf84345dff6d858fad1
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2021-05-29 17:16:29 -0300

    Rename function generate to generatePixels

--- app/Main.hs
+++ app/Main.hs
@@ -12,10 +12,12 @@ pixel pi pj = (convert pi, convert pj, convert 0.25)
 
 generate :: Integer -> Integer -> IO ()
 generate height width = mapM_ (\(i, j) -> do
+generatePixels :: Integer -> Integer -> IO ()
+generatePixels height width = mapM_ (\(i, j) -> do
     let pi = (fromIntegral i) / (fromIntegral height-1)
     let pj = (fromIntegral j) / (fromIntegral width-1)
     let (px, py, pz) = pixel pi pj
     putStr $ printf "%d %d %d\n" px py pz) [(i, j) | i <- [0..height-1], j <- [0..width-1]]
 
 main :: IO ()
-main = generate 2 2
+main = generatePixels 2 2
