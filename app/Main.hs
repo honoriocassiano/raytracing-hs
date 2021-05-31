9557eaace157c38e47f35ebac54c205ea8620999
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2021-05-31 07:35:22 -0300

    Rename functions
    - Rename function generatePixels to pixel
    - Rename function generateHeader to header

--- app/Main.hs
+++ app/Main.hs
@@ -19,21 +19,21 @@ infix 7 /~ -- Same precedence of / operator
 convert :: Float -> Integer
 convert v = truncateFloatInteger (v * 255.999)
 
-generatePixel :: Integer -> Integer -> ((Integer, Integer) -> Pixel)
-generatePixel h w =
+pixel :: Integer -> Integer -> ((Integer, Integer) -> Pixel)
+pixel h w =
     (\(i, j) -> Pixel (i /~ (w-1)) (j /~ (h-1)) 0.25)
 
-generateHeader :: Integer -> Integer -> [String]
-generateHeader h w = ["P3",
-                      (show w) ++ " " ++ (show h),
-                      "255"]
+header :: Integer -> Integer -> [String]
+header h w = ["P3",
+              (show w) ++ " " ++ (show h),
+              "255"]
 
-generatePixels :: Integer -> Integer -> [Pixel]
-generatePixels h w = map (generatePixel h w) [(i, j) | j <- reverse [0..h-1],
-                                                       i <- [0..w-1]]
+pixels :: Integer -> Integer -> [Pixel]
+pixels h w = map (pixel h w) [(i, j) | j <- reverse [0..h-1],
+                                       i <- [0..w-1]]
 
 generate :: Integer -> Integer -> [String]
-generate h w = (generateHeader h w) ++ map show (generatePixels h w)
+generate h w = (header h w) ++ map show (pixels h w)
 
 save :: String -> [String] -> IO ()
 save file content = writeFile file (intercalate "\n" content)
